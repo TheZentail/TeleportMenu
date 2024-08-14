79c330ded9ec3ef823d0e2de7685f4ce6f974188
Tanner <tanner@tannerplumb.com>
2024-08-13 20:56:03 -0700

    Add files via upload

---
+++ TeleportData.lua
@@ -0,0 +1,332 @@
+TeleportData = TeleportData or {}
+
+TeleportData["Current Season"] = {
+    ["Dungeons"] = {
+        [1] = "Algeth'ar Academy",
+        [2] = "Azure Vault",
+        [3] = "Brackenhide Hollow",
+        [4] = "Dawn of the Infinite",
+        [5] = "Halls of Infusion",
+        [6] = "Neltharus",
+        [7] = "Nokhud Offensive",
+        [8] = "Ruby Life Pools",
+        [9] = "Uldaman: Legacy of Tyr"
+    },
+    ["Raids"] = {
+        [1] = "Aberrus, the Shadowed Crucible",
+        [2] = "Amirdrassil, the Dream's Hope",
+        [3] = "Vault of the Incarnates"
+    }
+}
+
+TeleportData["Pathways"] = {
+    [1] = {
+        ["Expansion"] = "Classic"
+    },
+    [2] = {
+        ["Expansion"] = "Burning Crusade"
+    },
+    [3] = {
+        ["Expansion"] = "Wrath of the Lich King"
+    },
+    [4] = {
+        ["Expansion"] = "Cataclysm",
+        ["Dungeons"] = {
+            [1] = {
+                ["Name"] = "Grim Batol",
+                ["Spell"] = "Path of the Grim Batol"
+            },
+            [2] = {
+                ["Name"] = "Throne of the Tides",
+                ["Spell"] = "Path of the Tidehunter"
+            },
+            [3] = {
+                ["Name"] = "Vortex Pinnacle",
+                ["Spell"] = "Path of Wind's Domain"
+            }
+        }
+    },
+    [5] = {
+        ["Expansion"] = "Mists of Pandaria",
+        ["Dungeons"] = {
+            [1] = {
+                ["Name"] = "Gate of the Setting Sun",
+                ["Spell"] = "Path of the Setting Sun"
+            },
+            [2] = {
+                ["Name"] = "Mogu'shan Palace",
+                ["Spell"] = "Path of the Mogu King"
+            },
+            [3] = {
+                ["Name"] = "Scarlet Halls",
+                ["Spell"] = "Path of the Scarlet Blade"
+            },
+            [4] = {
+                ["Name"] = "Scarlet Monastery",
+                ["Spell"] = "Path of the Scarlet Mitre"
+            },
+            [5] = {
+                ["Name"] = "Scholomance",
+                ["Spell"] = "Path of the Necromancer"
+            },
+            [6] = {
+                ["Name"] = "Shadow-Pan Monastery",
+                ["Spell"] = "Path of the Shado-Pan"
+            },
+            [7] = {
+                ["Name"] = "Siege of Niuzao Temple",
+                ["Spell"] = "Path of the Black Ox"
+            },
+            [8] = {
+                ["Name"] = "Stormstout Brewery",
+                ["Spell"] = "Path of the Stout Brew"
+            },
+            [9] = {
+                ["Name"] = "Temple of the Jade Serpent",
+                ["Spell"] = "Path of the Jade Serpent"
+            }
+        }
+    },
+    [6] = {
+        ["Expansion"] = "Warlords of Draenor",
+        ["Dungeons"] = {
+            [1] = {
+                ["Name"] = "Auchindoun",
+                ["Spell"] = "Path of the Vigilant"
+            },
+            [2] = {
+                ["Name"] = "Bloodmaul Slag Mines",
+                ["Spell"] = "Path of the Bloodmaul"
+            },
+            [3] = {
+                ["Name"] = "Everbloom",
+                ["Spell"] = "Path of the Verdant"
+            },
+            [4] = {
+                ["Name"] = "Grimrail Depot",
+                ["Spell"] = "Path of the Dark Rail"
+            },
+            [5] = {
+                ["Name"] = "Iron Docks",
+                ["Spell"] = "Path of the Iron Paw"
+            },
+            [6] = {
+                ["Name"] = "Shadowmoon Burial Grounds",
+                ["Spell"] = "Path of the Crescent Moon"
+            },
+            [7] = {
+                ["Name"] = "Skyreach",
+                ["Spell"] = "Path of the Skies"
+            },
+            [8] = {
+                ["Name"] = "Upper Blackrock Spire",
+                ["Spell"] = "Path of the Burning Mountain"
+            }
+        }
+    },
+    [7] = {
+        ["Expansion"] = "Legion",
+        ["Dungeons"] = {
+            [1] = {
+                ["Name"] = "Black Rook Hold",
+                ["Spell"] = "Path of Ancient Horrors"
+            },
+            [2] = {
+                ["Name"] = "Court of Stars",
+                ["Spell"] = "Path of the Grand Magistrix"
+            },
+            [3] = {
+                ["Name"] = "Darkheart Thicket",
+                ["Spell"] = "Path of the Nightmare Lord"
+            },
+            [4] = {
+                ["Name"] = "Halls of Valor",
+                ["Spell"] = "Path of Proven Worth"
+            },
+            [5] = {
+                ["Name"] = "Karazhan",
+                ["Spell"] = "Path of the Fallen Guardian"
+            },
+            [6] = {
+                ["Name"] = "Neltharion's Lair",
+                ["Spell"] = "Path of the Earth-Warder"
+            }
+        }
+    },
+    [8] = {
+        ["Expansion"] = "Battle for Azeroth",
+        ["Dungeons"] = {
+            [1] = {
+                ["Name"] = "Atal'Dazar",
+                ["Spell"] = "Path of the Golden Tomb"
+            },
+            [2] = {
+                ["Name"] = "Freehold",
+                ["Spell"] = "Path of the Freebooter"
+            },
+            [3] = {
+                ["Name"] = "Operation: Mechagon",
+                ["Spell"] = "Path of the Scrappy Prince"
+            },
+            [4] = {
+                ["Name"] = "Siege of Boralus",
+                ["Spell"] = "Path of the Besieged Harbor"
+            },
+            [5] = {
+                ["Name"] = "Underrot",
+                ["Spell"] = "Path of Festering Rot"
+            },
+            [6] = {
+                ["Name"] = "Waycrest Manor",
+                ["Spell"] = "Path of Heart's Bane"
+            }
+        }
+    },
+    [9] = {
+        ["Expansion"] = "Shadowlands",
+        ["Dungeons"] = {
+            [1] = {
+                ["Name"] = "De Other Side",
+                ["Spell"] = "Path of the Scheming Loa"
+            },
+            [2] = {
+                ["Name"] = "Halls of Atonement",
+                ["Spell"] = "Path of the Sinful Soul"
+            },
+            [3] = {
+                ["Name"] = "Mists of Tirna Scithe",
+                ["Spell"] = "Path of the Misty Forest"
+            },
+            [4] = {
+                ["Name"] = "Necrotic Wake",
+                ["Spell"] = "Path of the Courageous"
+            },
+            [5] = {
+                ["Name"] = "Plaguefall",
+                ["Spell"] = "Path of the Plagued"
+            },
+            [6] = {
+                ["Name"] = "Sanguine Depths",
+                ["Spell"] = "Path of the Stone Warden"
+            },
+            [7] = {
+                ["Name"] = "Spires of Ascension",
+                ["Spell"] = "Path of the Ascendant"
+            },
+            [8] = {
+                ["Name"] = "Tazavesh, the Veiled Market",
+                ["Spell"] = "Path of the Streetwise Merchant"
+            },
+            [9] = {
+                ["Name"] = "Theater of Pain",
+                ["Spell"] = "Path of the Undefeated"
+            }
+        },
+        ["Raids"] = {
+            [1] = {
+                ["Name"] = "Castle Nathria",
+                ["Spell"] = "Path of the Sire"
+            },
+            [2] = {
+                ["Name"] = "Sanctum of Domination",
+                ["Spell"] = "Path of the Tormented Soul"
+            },
+            [3] = {
+                ["Name"] = "Seplucher of the First Ones",
+                ["Spell"] = "Path of the First Ones"
+            }
+        }
+    },
+    [10] = {
+        ["Expansion"] = "Dragonflight",
+        ["Dungeons"] = {
+            [1] = {
+                ["Name"] = "Algeth'ar Academy",
+                ["Spell"] = "Path of the Draconic Diploma"
+            },
+            [2] = {
+                ["Name"] = "Azure Vault",
+                ["Spell"] = "Path of Arcane Secrets"
+            },
+            [3] = {
+                ["Name"] = "Brackenhide Hollow",
+                ["Spell"] = "Path of the Rotting Woods"
+            },
+            [4] = {
+                ["Name"] = "Dawn of the Infinite",
+                ["Spell"] = "Path of Twisted Time"
+            },
+            [5] = {
+                ["Name"] = "Halls of Infusion",
+                ["Spell"] = "Path of the Titanic Reservoir"
+            },
+            [6] = {
+                ["Name"] = "Neltharus",
+                ["Spell"] = "Path of the Obsidian Hoard"
+            },
+            [7] = {
+                ["Name"] = "Nokhud Offensive",
+                ["Spell"] = "Path of the Windswept Plains"
+            },
+            [8] = {
+                ["Name"] = "Ruby Life Pools",
+                ["Spell"] = "Path of the Clutch Defender"
+            },
+            [9] = {
+                ["Name"] = "Uldaman: Legacy of Tyr",
+                ["Spell"] = "Path of the Watcher's Legacy"
+            }
+        },
+        ["Raids"] = {
+            [1] = {
+                ["Name"] = "Aberrus, the Shadowed Crucible",
+                ["Spell"] = "Path of the Bitter Legacy"
+            },
+            [2] = {
+                ["Name"] = "Amirdrassil, the Dream's Hope",
+                ["Spell"] = "Path of the Scorching Dream"
+            },
+            [3] = {
+                ["Name"] = "Vault of the Incarnates",
+                ["Spell"] = "Path of the Primal Prison"
+            }
+        }
+    },
+    [11] = {
+        ["Expansion"] = "The War Within",
+        ["Dungeons"] = {
+            [1] = {
+                ["Name"] = "Path of the Ara-Kara, City of Echoes",
+                ["Spell"] = "Path of the Ara-Kara, City of Echoes"
+            },
+            [2] = {
+                ["Name"] = "Path of the Brewery",
+                ["Spell"] = "Path of the Brewery"
+            },
+            [3] = {
+                ["Name"] = "Path of the City of Threads",
+                ["Spell"] = "Path of the City of Threads"
+            },
+            [4] = {
+                ["Name"] = "Path of the Darkflame Cleft",
+                ["Spell"] = "Path of the Darkflame Cleft"
+            },
+            [5] = {
+                ["Name"] = "Path of the Dawnbreaker",
+                ["Spell"] = "Path of the Dawnbreaker"
+            },
+            [6] = {
+                ["Name"] = "Path of the Priory of the Sacred Flame",
+                ["Spell"] = "Path of the Priory of the Sacred Flame"
+            },
+            [7] = {
+                ["Name"] = "Path of the Rookery",
+                ["Spell"] = "Path of the Rookery"
+            },
+            [8] = {
+                ["Name"] = "Path of the Stonevault",
+                ["Spell"] = "Path of the Stonevault"
+            }
+        }
+    }
+}
---
+++ TeleportMenu.lua
@@ -0,0 +1,206 @@
+-- local scrollable = true
+local posX, posY = 450, 0
+
+function HasExpansionDungeons(xpac)
+    for _, expansions in ipairs(TeleportData["Pathways"]) do
+        if expansions["Expansion"] == xpac then
+            for _, dungeon in ipairs(expansions["Dungeons"]) do
+                if C_Spell.DoesSpellExist(dungeon["Spell"]) then
+                    return true
+                end
+            end
+        end
+    end
+end
+
+function HasExpansionRaids(xpac)
+    for _, expansions in ipairs(TeleportData["Pathways"]) do
+        if expansions["Expansion"] == xpac then
+            for _, raid in ipairs(expansions["Raids"]) do
+                if C_Spell.DoesSpellExist(raid["Spell"]) then
+                    return true
+                end
+            end
+        end
+    end
+end
+
+function HasAnyDungeons()
+    for _, expansion in ipairs(TeleportData["Pathways"]) do
+        if expansion["Dungeons"] then
+            for _, dungeon in ipairs(expansion["Dungeons"]) do
+                if C_Spell.DoesSpellExist(dungeon["Spell"]) then
+                    return true
+                end
+            end
+        end
+    end
+end
+
+function HasAnyRaids()
+    for _, expansion in ipairs(TeleportData["Pathways"]) do
+        if expansion["Raids"] then
+            for _, raid in ipairs(expansion["Raids"]) do
+                if C_Spell.DoesSpellExist(raid["Spell"]) then
+                    return true
+                end
+            end
+        end
+    end
+end
+
+function HasCurrentSeasonDungeons()
+    for _, dungeonName in ipairs(TeleportData["Current Season"]["Dungeons"]) do
+        for _, expansions in ipairs(TeleportData["Pathways"]) do
+            if expansions["Dungeons"] then
+                for _, dungeon in ipairs(expansions["Dungeons"]) do
+                    if dungeon["Name"] == dungeonName then
+                        if C_Spell.DoesSpellExist(dungeon["Spell"]) then
+                            return true
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+function HasCurrentSeasonRaids()
+    for _, raidName in ipairs(TeleportData["Current Season"]["Raids"]) do
+        for _, expansions in ipairs(TeleportData["Pathways"]) do
+            if expansions["Raids"] then
+                for _, raid in ipairs(expansions["Raids"]) do
+                    if raid["Name"] == raidName then
+                        if C_Spell.DoesSpellExist(raid["Spell"]) then
+                            return true
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+function ExpansionHasRaids(category)
+    for _, expansions in ipairs(TeleportData["Pathways"]) do
+        if expansions["Expansion"] == category then
+            if expansions["Raids"] then
+                return true
+            end
+        end
+    end
+end
+
+function CreateSpellButton(menu, btnName, spellName)
+    local btnFrame = menu:CreateTemplate("SecureActionButtonTemplate")
+    btnFrame:AddInitializer(function(btn, desc, menu)
+        btn:SetText(btnName)
+        btn:GetFontString():SetPoint("LEFT", 0, 0)
+        local width = btn:GetFontString():GetStringWidth() - 20
+        local height = 20
+        btn:SetSize(width, height)
+        btn:SetNormalFontObject("GameFontNormal")
+        btn:SetHighlightFontObject("GameFontHighlight")
+        -- btn:SetNormalTexture("Interface/Buttons/UI-Panel-Button-Up")
+        -- btn:SetHighlightTexture("Interface/Buttons/UI-Panel-Button-Highlight")
+        -- btn:SetPushedTexture("Interface/Buttons/UI-Panel-Button-Down")
+        -- btn:SetDisabledTexture("Interface/Buttons/UI-Panel-Button-Disabled")
+        btn:SetAttribute("type", "macro")
+        btn:SetAttribute("macrotext", "/cast " .. spellName)
+        btn:RegisterForClicks("AnyUp", "AnyDown")
+    end)
+end
+
+local Dropdown = CreateFrame("DropdownButton", nil, UIParent, "WowStyle1DropdownTemplate")
+Dropdown:SetDefaultText("Teleports")
+Dropdown:SetPoint("BOTTOMLEFT", posX, posY)
+Dropdown:SetMovable(true)
+Dropdown:EnableMouse(true)
+Dropdown:RegisterForDrag("LeftButton")
+Dropdown:SetScript("OnDragStart", Dropdown.StartMoving)
+Dropdown:SetScript("OnDragStop", Dropdown.StopMovingOrSizing)
+Dropdown:SetClampedToScreen(true)
+
+-- on Dropdown mouseover, hide any current tooltips
+Dropdown:SetScript("OnEnter", function(self)
+    GameTooltip:Hide()
+end)
+
+Dropdown:SetupMenu(function(dropdown, rootDescription)
+    ----- HEARTHSTONE -----
+
+    ----- DUNGEONS -----
+    if HasAnyDungeons() then
+        local dungeons = rootDescription:CreateButton("Dungeons")
+
+        if HasCurrentSeasonDungeons() then
+            local current = dungeons:CreateButton("Current Season")
+            for _, dungeonName in ipairs(TeleportData["Current Season"]["Dungeons"]) do
+                for _, expansions in ipairs(TeleportData["Pathways"]) do
+                    if expansions["Dungeons"] then
+                        for _, dungeon in ipairs(expansions["Dungeons"]) do
+                            if dungeon["Name"] == dungeonName then
+                                if C_Spell.DoesSpellExist(dungeon["Spell"]) then
+                                    CreateSpellButton(current, dungeon["Name"], dungeon["Spell"])
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+            dungeons:CreateDivider()
+        end
+
+        for _, expansions in ipairs(TeleportData["Pathways"]) do
+            if expansions["Dungeons"] then
+                if HasExpansionDungeons(expansions["Expansion"]) then
+                    local xpac = dungeons:CreateButton(expansions["Expansion"])
+                    for _, dungeon in ipairs(expansions["Dungeons"]) do
+                        if C_Spell.DoesSpellExist(dungeon["Spell"]) then
+                            CreateSpellButton(xpac, dungeon["Name"], dungeon["Spell"])
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    ----- RAIDS -----
+    if HasAnyRaids() then
+        local raids = rootDescription:CreateButton("Raids")
+        if HasCurrentSeasonRaids() then
+            local current = raids:CreateButton("Current Season")
+            for _, raidName in ipairs(TeleportData["Current Season"]["Raids"]) do
+                for _, expansions in ipairs(TeleportData["Pathways"]) do
+                    if expansions["Raids"] then
+                        for _, raid in ipairs(expansions["Raids"]) do
+                            if raid["Name"] == raidName then
+                                if C_Spell.DoesSpellExist(raid["Spell"]) then
+                                    CreateSpellButton(current, raid["Name"], raid["Spell"])
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+            raids:CreateDivider()
+        end
+
+        for _, expansions in ipairs(TeleportData["Pathways"]) do
+            if expansions["Raids"] then
+                if HasExpansionRaids(expansions["Expansion"]) then
+                    local xpac = raids:CreateButton(expansions["Expansion"])
+                    for _, raid in ipairs(expansions["Raids"]) do
+                        if C_Spell.DoesSpellExist(raid["Spell"]) then
+                            CreateSpellButton(xpac, raid["Name"], raid["Spell"])
+                        end
+                    end
+                end
+            end
+        end
+    end
+end)
+
+function TeleportMenuMiniButtonClick()
+    print("Mini button")
+end
